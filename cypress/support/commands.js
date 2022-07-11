774f53041a140222dcf6c07bb41fef7d74ee21cd
Victor Georges <victor.george@valtech.com>
2022-07-11 11:20:49 -0300

    Click, List and Verify

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('accessListSite', () => { 
+    cy.get(`:nth-child(1) > .collapsible-header`).click().should(`be.visible`,`Lista de Usuários`) //click on menu to obtain `Lista de Usuários`
+    cy.get(`.active > .collapsible-body > ul > :nth-child(2) > a`).click().should(`be.visible`,`Lista de Usuários!!`) //click on `Lista de Usuários` and load page
+})
