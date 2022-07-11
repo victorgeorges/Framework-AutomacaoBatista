159e5be26c878ccd9a429abf60addd944f6a564a
Victor Georges <victor.george@valtech.com>
2022-07-11 11:36:17 -0300

    Bugs Fix

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,9 @@
-
 import '@4tw/cypress-drag-drop'
-
-
 Cypress.Commands.add('accessSite', () => { //add "access site" custom command
     cy.clock(setTimeout)//freeze the clock
     cy.visit(Cypress.env('url'))  //visit homepage
     cy.tick()//unfreeze the clock
 })
-
-
-
 
 Cypress.Commands.add('accessListSite', () => { 
     cy.get(`:nth-child(1) > .collapsible-header`).click().should(`be.visible`,`Lista de Usuários`) //click on menu to obtain `Lista de Usuários`
